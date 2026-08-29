2d816a752d3b5334fca2289efa13fa03a2d420a5
leonardoscheller-star <leonardo.scheller@escola.pr.gov.br>
2026-08-29 16:22:50 -0300

    salvar

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ texto.textContent--;
 curtiu = false;
 }
 }
+const botoes = document.querySelectorAll('button');
+botoes.forEach(function(botao) {
+    let jaCurtiu = false;
+    botao.addEventListener('click', function() {
+        let texto = botao.querySelector('span');
+        if (texto) {
+            let numeroAtual = parseInt(texto.textContent) || 0;
+            if (jaCurtiu === false) {
+                texto.textContent = numeroAtual + 1;//se ainda nao tiver curtido e apertar adiciona 1 e jaCurtiu fica verdadeiro
+                jaCurtiu = true;
+            } else {
+                texto.textContent = numeroAtual - 1;
+                jaCurtiu = false;
+            }
+        }
+    });
+});
